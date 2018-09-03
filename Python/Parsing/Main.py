d64f0cb674033ff3e9ccf42935ea7d036de03a59
leff9f <leff9f@gmail.com>
2018-09-03 21:31:47 +0700

    03.09.18 changed data save after checking, created list TextSum.

--- Python/Parsing/Main.py
+++ Python/Parsing/Main.py
@@ -4,18 +4,19 @@ from data import image_search, image_download, data_import
 from data import save_num_sync, data_save, input_check
 import time
 
-# время выполнения
-tic = [time.time()]
-
 # подсчет числа заказных номеров подготовленных для запросов
 part_num_length = data_num()
 print('number of data: '+str(part_num_length))
 print('last used num: '+str(open_last_num()))
-# импорт заказного номера для последующей обработки
-part_num = data_import(open_last_num())
 
 # ввод числа считываемых данных
 x = input_check()
+
+# время выполнения
+tic = [time.time()]
+
+# импорт заказного номера для последующей обработки
+part_num = data_import(open_last_num())
 
 # запрос и запись данных
 i = 0
@@ -29,37 +30,21 @@ while i < int(x):
                 if img_src != '':
                         image_download(img_src, part_num)
                 t1 = Terminals(part_num, data[0], data[1], data[2], data[3])
-                data_save(i + 2, 1, t1.part_num)
-                data_save(i + 2, 2, t1.type_num())
-                data_save(i + 2, 3, t1.desc)
-                data_save(i + 2, 4, t1.MANUFACTURE)
-                data_save(i + 2, 5, t1.MANUFACTURE)
-                data_save(i + 2, 6, t1.part_num)
-                data_save(i + 2, 7, t1.cat_page())
-                data_save(i + 2, 8, t1.pack_unit())
-                data_save(i + 2, 9, t1.UNIT)
-                data_save(i + 2, 10, t1.weight())
-                data_save(i + 2, 11, t1.width())
-                data_save(i + 2, 12, t1.height())
-                data_save(i + 2, 13, t1.depth())
-                data_save(i + 2, 14, t1.graph_file())
-                data_save(i + 2, 15, t1.colour())
-                data_save(i + 2, 16, t1.material())
-                data_save(i + 2, 17, t1.cross_sect())
-                data_save(i + 2, 18, t1.cross_min())
-                data_save(i + 2, 19, t1.cross_max())
-                data_save(i + 2, 20, t1.current())
-                data_save(i + 2, 21, t1.voltage())
-                save_num_sync(str(last_used_num))
-                print(t1.part_num)
                 tic.append(time.time())
-                print('calculation time: '+str(int(tic[i+1] - tic[0]))+' seconds')
+                print('calculation time: ' + str(int(tic[i + 1] - tic[0])) + ' seconds')
+                TextSum = [t1.part_num, t1.type_num(), t1.desc, t1.MANUFACTURE, t1.MANUFACTURE, t1.part_num,
+                           t1.cat_page(), t1.pack_unit(), t1.UNIT, t1.weight(), t1.width(), t1.height(),
+                           t1.depth(), t1.graph_file(), t1.colour(), t1.material(), t1.cross_sect(),
+                           t1.cross_min(), t1.cross_max(), t1.current(), t1.voltage()]
+                save_num_sync(str(last_used_num))
+                print(TextSum)
+                tic.append(time.time())
+                print('calculation time: '+str(int(tic[i+2] - tic[0]))+' seconds')
         else:
                 print('no data, something wrong')
         print('iteration: '+str(i))
         i += 1
 
 # сохранение последнего считанного заказного номер в файле temp
-
 input('bye')
 
